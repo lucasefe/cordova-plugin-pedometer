5dd6fe2b795a5ce5727798b8d507ef862854a193
Lucas Florio <lucasefe@gmail.com>
2023-01-08 15:03:57 -0300

    FIx

--- src/ios/Pedometer.m
+++ src/ios/Pedometer.m
@@ -57,8 +57,8 @@
                     @"numberOfSteps": [CMPedometer isStepCountingAvailable] && pedometerData.numberOfSteps ? pedometerData.numberOfSteps : [NSNumber numberWithInt:0],
                     @"distance": [CMPedometer isDistanceAvailable] && pedometerData.distance ? pedometerData.distance : [NSNumber numberWithInt:0],
                     @"floorsAscended": [CMPedometer isFloorCountingAvailable] && pedometerData.floorsAscended ? pedometerData.floorsAscended : [NSNumber numberWithInt:0],
-                    @"floorsDescended": [CMPedometer isFloorCountingAvailable] && pedometerData.floorsDescended ? pedometerData.floorsDescended : [NSNumber numberWithInt:0]
-                    @"currentCadence": [CMPedometer isStepCountingAvailable] && pedometerData.currentCadence ? pedometerData.currentCadence : [NSNumber numberWithInt:0]
+                    @"floorsDescended": [CMPedometer isFloorCountingAvailable] && pedometerData.floorsDescended ? pedometerData.floorsDescended : [NSNumber numberWithInt:0],
+                    @"currentCadence": [CMPedometer isStepCountingAvailable] && pedometerData.currentCadence ? pedometerData.currentCadence : [NSNumber numberWithInt:0],
                     @"currentPace": [CMPedometer isStepCountingAvailable] && pedometerData.currentPace ? pedometerData.currentPace: [NSNumber numberWithInt:0]
                 };
                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:pedestrianData];
